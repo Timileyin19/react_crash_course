bfa9ae0419e2e7594bf92c928d16d9b82b8bd2fd
timileyin19 <abioduntimileyin19@gmail.com>
2022-05-04 13:38:02 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [time, setTime] = useState(0);
+
+
+  const tick =  () => {
+    setTime(prevState => (prevState + 1 ))
+  }
+
+
+  useEffect(() => {
+    setInterval(() => tick(), 1000);
+  }, [])
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        Seconds: { time }
+      </div>
+
   );
 }
 
